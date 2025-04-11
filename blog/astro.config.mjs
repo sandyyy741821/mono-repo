20f12b01efe192178ed4565b388e1b91ef8bc96c
sandyyy741821 <santhoshsandyyy741821@gmail.com>
2025-04-11 17:16:28 +0530

    Fix: remove missing landing build from script

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://b7eb926f.mono-repo-cu9.pages.dev', 
+  site: 'https://mono-repo-cu9.pages.dev', 
   base: '/blog', 
   outDir: 'dist',
   integrations: [mdx(), sitemap()],
