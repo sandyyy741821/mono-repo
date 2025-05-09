101bb148b6e985b8fd44650a3017a62f1ed12f0c
sandyyy741821 <santhoshsandyyy741821@gmail.com>
2025-05-09 12:18:13 +0530

    updated config files

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://mono-repo-cu9.pages.dev',
-  base: '/', 
+  base: isR2 ? './' : '/', 
   build: {
     outDir: 'dist',
   },
